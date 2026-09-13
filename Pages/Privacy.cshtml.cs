afb554cfc6795177ef646b2dba6b0880125c0498
pgc10 <gc.prasant900@gmail.com>
2026-09-13 00:48:28 -0500

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1_VisualStudio.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
