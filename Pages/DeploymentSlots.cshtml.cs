7c52fcc2b53db5372f3cd01ad6dbf00292b2c594
Prashant G C <gc.prasant900@gmail.com>
2026-09-13 22:24:32 -0500

    Adding staging changes

---
+++ Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1_VisualStudio.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -14,7 +14,6 @@ namespace WebApplication1_VisualStudio.Pages
 
         public void OnGet()
         {
-            throw new Exception("Testing Insights");
         }
     }
 
